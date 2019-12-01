c22136a4f5310f57ef63a93358cdf46ea007d943
salatuk <58403588+salatuk@users.noreply.github.com>
2019-12-02 00:12:24 +0100

    first upload
    Upload new files

---
+++ precache-manifest.4fbe107b176d8c128c4f41327ca54728.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "289cda261d2a7d3e6e853b30e16d718a",
+    "url": "/athan/static/media/AraJozoor-Regular.289cda26.ttf"
+  },
+  {
+    "revision": "43879209b06ebb10c974",
+    "url": "/athan/static/css/main.57616310.chunk.css"
+  },
+  {
+    "revision": "90dd145a8f90ec741a21",
+    "url": "/athan/static/js/runtime~main.90dd145a.js"
+  },
+  {
+    "revision": "5a43dec2b91f6cbb4d6d62dfb83da50a",
+    "url": "/athan/static/media/009-cloudy-night.5a43dec2.svg"
+  },
+  {
+    "revision": "e6b586001b844e2ddb7f",
+    "url": "/athan/static/js/2.e6b58600.chunk.js"
+  },
+  {
+    "revision": "b573015b8452e0f4eb977bbfb193f03f",
+    "url": "/athan/static/media/AraJozoor-Regular.b573015b.woff"
+  },
+  {
+    "revision": "f2b8b184ba39f851059aeada97db517b",
+    "url": "/athan/static/media/AraJozoor-Regular.f2b8b184.svg"
+  },
+  {
+    "revision": "fa1f99dc4c40e5a082e31222a2f2436e",
+    "url": "/athan/static/media/AraJozoor-Regular.fa1f99dc.eot"
+  },
+  {
+    "revision": "43879209b06ebb10c974",
+    "url": "/athan/static/js/main.43879209.chunk.js"
+  },
+  {
+    "revision": "28157ec23ba997350570797d304ca708",
+    "url": "/athan/static/media/016-cloudy-night-3.28157ec2.svg"
+  },
+  {
+    "revision": "1f69a3e9e85c2ed02d73bfc813c0efec",
+    "url": "/athan/static/media/002-sunrise.1f69a3e9.svg"
+  },
+  {
+    "revision": "004ec82a40d4fadb92b08d52fc464dcb",
+    "url": "/athan/static/media/001-sun.004ec82a.svg"
+  },
+  {
+    "revision": "aa8cc3774681f3f899e2ac4105fac807",
+    "url": "/athan/static/media/015-cloudy-day-1.aa8cc377.svg"
+  },
+  {
+    "revision": "53a38aace3bcc6b79631ab4755ceba72",
+    "url": "/athan/static/media/003-sunset.53a38aac.svg"
+  },
+  {
+    "revision": "e6b586001b844e2ddb7f",
+    "url": "/athan/static/css/2.b49df0ea.chunk.css"
+  },
+  {
+    "revision": "b831c177645f6bccbf69d56581771f07",
+    "url": "/athan/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/athan/precache-manifest.4fbe107b176d8c128c4f41327ca54728.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/athan/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
